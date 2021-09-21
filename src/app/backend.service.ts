daaab68d544bda6d771f2ac4b2492679b0bdbf35
Bobby00 <edwarobert6@gmail.com>
2021-09-21 10:45:02 +0300

    changing apiurl

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -13,7 +13,7 @@ export class BackendService {
   constructor(private _http: HttpClient) { }
 
   // Make connection to backend server
-  apiUrl = 'http://localhost:3000/drinks'
+  apiUrl = 'https://git.heroku.com/cocktail-node-backend.git/drinks'
 
   // get all cocktails data in db
   getAllCocktails():Observable<any> {
